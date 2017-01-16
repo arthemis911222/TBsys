e0cb88ce2397ca23c603a950507bd194cd64700c
arthemis911222 <527028589@qq.com>
2017-01-16 17:56:18 +0800

    数据库建立，模板建立

---
+++ myTBsys/myTBsys/Areas/TBsys/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myTBsys.Areas.TBsys.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: TBsys/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ myTBsys/myTBsys/Areas/TBsys/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myTBsys.Areas.TBsys.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: TBsys/Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
