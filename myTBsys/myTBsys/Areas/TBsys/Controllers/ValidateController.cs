0ceb05d36bf1de783eef0c154e1088fffdbfed4e
arthemis911222 <527028589@qq.com>
2017-03-18 18:22:54 +0800

    教材征订1.3

---
+++ myTBsys/myTBsys/Areas/TBsys/Controllers/ValidateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myTBsys.Areas.TBsys.Controllers
+{
+    public class ValidateController : Controller
+    {
+        // GET: TBsys/Validate
+        /// <summary>
+        /// 验证码
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetValidateCode()
+        {
+            Other.ValidateCode vCode = new Other.ValidateCode();
+            string code = vCode.CreateValidateCode(4);
+            Session["ValidateCode"] = code;//关键点
+            byte[] bytes = vCode.CreateValidateGraphic(code);
+            return File(bytes, @"image/jpeg");
+        }
+    }
+}
